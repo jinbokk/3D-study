ada5947c185f046a7ff61f1538a0d377da646ffc
JINBOK LEE <eyelash1024@naver.com>
2023-08-14 01:25:11 +0900

    making 3D cube

--- 3D/1-cube/src/main.js
+++ 3D/1-cube/src/main.js
@@ -5,5 +5,54 @@ window.addEventListener("load", () => {
 });
 
 function init() {
-  console.log(THREE);
+  const renderer = new THREE.WebGLRenderer({
+    // alpha: true,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  const material = new THREE.MeshStandardMaterial({
+    color: new THREE.Color(0xcc99ff),
+    // color: "white",
+    // transparent: true,
+    // opacity: 0.5,
+    // wireframe: true,
+    // side: THREE.DoubleSide,
+  });
+
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.set(3, 4, 5);
+  camera.lookAt(cube.position);
+
+  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1);
+  directionalLight.position.set(-1, 2, 3);
+  scene.add(directionalLight);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+  scene.add(ambientLight);
+
+  renderer.render(scene, camera);
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
 }
