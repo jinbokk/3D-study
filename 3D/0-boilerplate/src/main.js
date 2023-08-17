108ae378d2d7906080f008a1f8ed727acf28d841
JINBOK LEE <eyelash1024@naver.com>
2023-08-18 01:19:38 +0900

    boiler plate minor update

--- 3D/0-boilerplate/src/main.js
+++ 3D/0-boilerplate/src/main.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 window.addEventListener("load", () => {
   init();
@@ -10,29 +9,31 @@ function init() {
     antialias: true,
   });
 
+  /** Renderer */
   renderer.setSize(window.innerWidth, window.innerHeight);
-
   document.body.appendChild(renderer.domElement);
 
+  /** Scene */
   const scene = new THREE.Scene();
 
+  /** Camera */
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     1,
     500
   );
+  camera.position.z = 5;
 
-  camera.position.set(2, 2, 4);
-
+  /** Render function */
   function render() {
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
-
   render();
 
+  /** Resize function */
   function handleResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
