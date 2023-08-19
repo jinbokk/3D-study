23bce5ce4e8a5f84dcf8352a295740b34e700195
JINBOK LEE <eyelash1024@naver.com>
2023-08-20 02:22:54 +0900

    gsap scrollTrigger added

--- 3D/4-origami_boat/src/main.js
+++ 3D/4-origami_boat/src/main.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import { OutlineEffect } from "three/examples/jsm/effects/OutlineEffect.js";
 
 window.addEventListener("load", () => {
@@ -10,6 +12,14 @@ window.addEventListener("load", () => {
 const canvas = document.querySelector("#canvas");
 
 async function init() {
+  /** Params */
+  const params = {
+    waveColor: "#00ffff",
+    wireframeColor: "gray",
+    backgroundColor: "#ffffff",
+    fogColor: "#f0f0f0",
+  };
+
   /** Renderer */
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
@@ -30,7 +40,7 @@ async function init() {
     1,
     500
   );
-  camera.position.set(0, 40, 170);
+  camera.position.set(0, 50, 180);
 
   /** Controls */
   // const controls = new OrbitControls(camera, renderer.domElement);
@@ -64,17 +74,15 @@ async function init() {
   };
 
   /** Wave */
-  const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 120, 120);
+  const waveGeometry = new THREE.PlaneGeometry(2000, 2000, 120, 102);
   const waveMaterial = new THREE.MeshStandardMaterial({
     // wireframe: true,
-    color: "#00ffff",
+    // color: "#00ffff",
+    color: params.waveColor,
   });
   const waveMesh = new THREE.Mesh(waveGeometry, waveMaterial);
   waveMesh.rotateX(-Math.PI / 2);
   waveMesh.receiveShadow = true;
-  // waveMesh.shadow.mapSize.width = 1024;
-  // waveMesh.shadow.mapSize.height = 1024;
-  // waveMesh.shadow.radius = 10;
   scene.add(waveMesh);
 
   // const waveWireframeGeometry = new THREE.WireframeGeometry(waveMesh);
@@ -86,13 +94,13 @@ async function init() {
   waveWireframe.material = waveMesh.material.clone();
   waveWireframe.material.wireframe = true;
   waveWireframe.material.transparent = true;
-  waveWireframe.material.color = new THREE.Color("gray");
+  waveWireframe.material.color = new THREE.Color(params.wireframeColor);
   waveWireframe.material.opacity = 0.1;
   scene.add(waveWireframe);
 
   /** Wave Height */
   const initialWaveHeight = [];
-  const waveHeight = 3;
+  const waveHeight = 3.5;
   for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
     const z =
       waveGeometry.attributes.position.getZ(i) +
@@ -112,7 +120,7 @@ async function init() {
   };
 
   /** Fog */
-  scene.fog = new THREE.FogExp2(0xffffff, 0.005);
+  scene.fog = new THREE.FogExp2(params.fogColor, 0.005);
   // scene.fog = new THREE.Fog(0xffffff, 0, 300);
 
   /** Ambient Light */
@@ -120,12 +128,12 @@ async function init() {
   scene.add(ambientLight);
 
   /** Point Light */
-  const pointLight = new THREE.PointLight(0xffffff, 6000);
+  const pointLight = new THREE.PointLight(0xffffff, 10000);
   pointLight.position.set(-15, 60, 150);
   pointLight.castShadow = true;
   pointLight.shadow.mapSize.width = 1024;
   pointLight.shadow.mapSize.height = 1024;
-  pointLight.shadow.radius = 10;
+  pointLight.shadow.radius = 5;
   scene.add(pointLight);
   // const pointLightHelper = new THREE.PointLightHelper(pointLight, 5, "#ff0000");
   // scene.add(pointLightHelper);
@@ -133,6 +141,9 @@ async function init() {
   /** Directional Light */
   const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
   directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 1024;
+  directionalLight.shadow.mapSize.height = 1024;
+  directionalLight.shadow.radius = 5;
   scene.add(directionalLight);
 
   /** Render function */
@@ -142,6 +153,7 @@ async function init() {
     renderer.render(scene, camera);
     waveMesh.update();
     boatMesh.update();
+    // camera.lookAt(0, 0, 70);
 
     requestAnimationFrame(render);
   }
@@ -153,8 +165,81 @@ async function init() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
+    // renderer.render(scene, camera);
   }
 
   window.addEventListener("resize", handleResize);
+
+  /** ScrollTrigger */
+  gsap.registerPlugin(ScrollTrigger);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".wrapper",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: true,
+    },
+  });
+
+  tl.to(params, {
+    waveColor: "#4268ff",
+    wireframeColor: "white",
+    onUpdate: () => {
+      waveMaterial.color = new THREE.Color(params.waveColor);
+      waveWireframe.material.color = new THREE.Color(params.wireframeColor);
+    },
+    duration: 1.5,
+  })
+    .to(
+      params,
+      {
+        backgroundColor: "#2a2a2a",
+        onUpdate: () => {
+          scene.background = new THREE.Color(params.backgroundColor);
+        },
+        duration: 1.5,
+      },
+      "<"
+    )
+    .to(
+      params,
+      {
+        fogColor: "#2a2a2a",
+        onUpdate: () => {
+          scene.fog.color = new THREE.Color(params.fogColor);
+        },
+        duration: 1.5,
+      },
+      "<"
+    );
+  // .to(camera.position, {
+  //   x: 100,
+  //   z: 150,
+  //   duration: 2.5,
+  // })
+  // .to(boatMesh.position, {
+  //   z: 150,
+  //   duration: 2,
+  // })
+  // .to(camera.position, {
+  //   x: -100,
+  //   duration: 2,
+  // })
+  // .to(camera.position, {
+  //   x: 0,
+  //   y: 40,
+  //   z: 170,
+  //   duration: 2,
+  // });
+
+  gsap.to(".title", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".wrapper",
+      scrub: true,
+      pin: true,
+      end: "+1000",
+    },
+  });
 }
